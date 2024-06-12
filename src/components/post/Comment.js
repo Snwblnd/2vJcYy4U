2f627b907b16767171532f130724fad0ac15c161
Paul Ebert <paul.ebert@code.berlin>
2024-06-12 13:56:32 +0200

    add mock commentloader and expand comments

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -1,5 +1,27 @@
+import React, { useState } from "react";
+
 const Comment = ({ comment }) => {
-  return <div className="comment">{comment}</div>;
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="comment">
+      <div className="comment-text">
+        {comment.isDeleted ? (
+          <p>This comment has been deleted</p>
+        ) : (
+          <p>{comment.commentText}</p>
+        )}
+      </div>
+      <div className="replies">
+        {comment.comments && (
+          <button onClick={() => setExpanded(!expanded)}>
+            {expanded ? "Hide replies" : "Show replies"}
+          </button>
+        )}
+      </div>
+      <button>Delete</button>
+    </div>
+  );
 };
 
 export default Comment;
--- src/components/post/CommentSection.js
+++ src/components/post/CommentSection.js
@@ -8,9 +8,9 @@ const CommentSection = () => {
   return (
     <div className="comment-section">
       <div className="comments">
-        {comments.map((comment, index) => {
+        {comments.map((comment) => {
           console.log(comment);
-          return <Comment key={index} comment={comment} />;
+          return <Comment key={comment.commentId} comment={comment} />;
         })}
       </div>
       <CommentInput />
@@ -21,7 +21,37 @@ const CommentSection = () => {
 export default CommentSection;
 
 const commentLoader = () => {
-  const comments = ["this looks dope!", "can't wait to try!"];
+  const comments = [
+    {
+      commentId: 1,
+      commentText: "this looks great!",
+      isDeleted: false,
+      comments: [
+        {
+          commentId: 3,
+          commentText: "i agree!",
+          isDeleted: false,
+          comments: [
+            {
+              commentId: 5,
+              commentText: "love your username",
+              isDeleted: false,
+            },
+          ],
+        },
+        {
+          commentId: 4,
+          commentText: "this looks great!",
+          isDeleted: false,
+        },
+      ],
+    },
+    {
+      commentId: 2,
+      commentText: "can't wait to try this!",
+      isDeleted: false,
+    },
+  ];
 
   return comments;
 };
