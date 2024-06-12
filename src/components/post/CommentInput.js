9691a94757e33643f574d332d76f71dadaa5d47f
Paul Ebert <paul.ebert@code.berlin>
2024-06-12 16:21:49 +0200

    add addcomment func and clearcomments

--- src/components/post/CommentInput.js
+++ src/components/post/CommentInput.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+import { commentInit, updateComments } from "./PostUtils";
 import "./post.css";
 
 const CommentInput = () => {
-  return <div className="comment-input">Comment Input</div>;
+  const [comment, setComment] = useState("");
+
+  const handleInputChange = (event) => {
+    setComment(event.target.value);
+  };
+  return (
+    <div className="comment-input">
+      <input
+        className="reply-field"
+        type="text"
+        value={comment}
+        onChange={handleInputChange}
+      />
+      <button
+        className="submit-comment-button"
+        onClick={() => addComment(comment)}
+      >
+        submit
+      </button>
+      <button className="reset-comments-button" onClick={() => clearComments()}>
+        Reset comment section
+      </button>
+    </div>
+  );
+};
+
+const addComment = (commentText) => {
+  const commentId = Number(localStorage.getItem("idCounter")) + 1;
+  const newComment = {
+    commentId,
+    commentText,
+    isDeleted: false,
+  };
+
+  const comments = JSON.parse(localStorage.getItem("comments")) || [];
+
+  comments.push(newComment);
+
+  updateComments(comments);
+  localStorage.setItem("idCounter", commentId);
+};
+
+const clearComments = () => {
+  localStorage.removeItem("ranOnce");
+  localStorage.removeItem("comments");
+  localStorage.removeItem("idCounter");
+
+  commentInit();
+  window.dispatchEvent(new CustomEvent("updateList"));
 };
 
 export default CommentInput;
